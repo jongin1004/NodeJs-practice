52408de77bbb761137eb820631108f2463c4fbfe
jongin1004 <bill1224@naver.com>
2022-01-13 00:37:07 +0900

    read file and show content

--- main.js
+++ main.js
@@ -5,15 +5,39 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    // console.log();
+    let title = queryData.id;
     if(_url == '/'){
-      _url = '/index.html';
+      title = 'WELCOME';
     }
     if(_url == '/favicon.ico'){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(queryData.id);
+
+    
+    fs.readFile('./data/'+title, 'utf8', (err, content) => {
+      let template = `
+        <!doctype html>
+        <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="index.html">WEB</a></h1>
+            <ol>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ol>
+            <h2>${title}</h2>
+            <p>${content}</p>
+          </body>
+        </html>
+      `
+
+      response.end(template);
+    });    
  
 });
 app.listen(3000);
