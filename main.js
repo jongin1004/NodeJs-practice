9d08ad3cca30ad6ee6245f0cfcb270aa0f93d2fa
jongin1004 <bill1224@naver.com>
2022-01-13 00:07:20 +0900

    read file

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    console.log(queryData.id);
+    // console.log();
     if(_url == '/'){
       _url = '/index.html';
     }
@@ -13,7 +13,7 @@ var app = http.createServer(function(request,response){
       return response.writeHead(404);
     }
     response.writeHead(200);
-    response.end(fs.readFileSync(__dirname + _url));
+    response.end(queryData.id);
  
 });
 app.listen(3000);
