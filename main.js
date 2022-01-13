e4f3b81404486f9811864531e7646a614df21fa6
jongin1004 <bill1224@naver.com>
2022-01-13 16:20:02 +0900

    create new file using writeFile()

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let http = require('http');
 let fs = require('fs');
 let url = require('url');
+let qs = require('querystring');
 
 function templateHTML(title, fileList, content) {
   return `
@@ -55,7 +56,7 @@ let app = http.createServer(function(request,response){
       fs.readdir('./data/', (err, _fileList) => {
         let fileList = getFileList(_fileList );
         let template = templateHTML(title = 'create', fileList, `
-          <form action="http://localhost:3000/process_create" method="post">
+          <form action="http://localhost:3000/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
               <textarea name="description" placeholder="description"></textarea>
@@ -69,6 +70,23 @@ let app = http.createServer(function(request,response){
         response.writeHead(200);
         response.end(template);
       });
+    } else if (pathname === '/create_process') {
+      let body ='';
+      // post방식으로 전송된 데이터를 조금씩 나눠서 받는다. (한꺼번에 많이 들어왔을 때 서버가 다운되는 것을 방지하기 위해서)
+      request.on('data', function(data) {
+        body += data;
+      });
+      // 더이상 들어올 정보가 없을 땐, end쪽이 실행된다. 
+      request.on('end', function() {
+        let post = qs.parse(body);
+        let title = post.title;
+        let desc = post.description;
+        fs.writeFile(`./data/${title}`, 'utf8', (err) => {
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end();
+        });
+      });
+      
     } else {
       response.writeHead(404);
       response.end('Not found');
