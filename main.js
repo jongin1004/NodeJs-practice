30660a5835dc696b085834c96f8c06ea927a3860
jongin1004 <bill1224@naver.com>
2022-01-13 15:20:01 +0900

    get list of file in dir

--- main.js
+++ main.js
@@ -1,43 +1,50 @@
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
+let http = require('http');
+let fs = require('fs');
+let url = require('url');
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    let title = queryData.id;
-    if(_url == '/'){
-      title = 'WELCOME';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-
+let app = http.createServer(function(request,response){
+    let _url = request.url;
+    let queryData = url.parse(_url, true).query;
+    let pathname = url.parse(_url, true).pathname;
+    let title = queryData.id;    
     
-    fs.readFile('./data/'+title, 'utf8', (err, content) => {
-      let template = `
-        <!doctype html>
-        <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="index.html">WEB</a></h1>
-            <ol>
-              <li><a href="/?id=HTML">HTML</a></li>
-              <li><a href="/?id=CSS">CSS</a></li>
-              <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ol>
-            <h2>${title}</h2>
-            <p>${content}</p>
-          </body>
-        </html>
-      `
+    if (pathname === '/') {
+      fs.readFile('./data/'+title, 'utf8', (err, content) => {
+        title = title !== undefined ? title : 'welcome';
+        content = title !== undefined ? content : 'hello, nodeJs';
+        
+        fs.readdir('./data/', (err, _fileList) => {
+          let fileList = '';
+          for (let i = 0; i < _fileList.length; i++) {
+            fileList += `<li><a href="/?id=${_fileList[i]}">${_fileList[i]}</a></li>`;
+          }
+          let template = `
+            <!doctype html>
+            <html>
+              <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+              </head>
+              <body>
+                <h1><a href="/">WEB</a></h1>
+                <ol>
+                  ${fileList}      
+                </ol>
+                <h2>${title}</h2>
+                <p>${content}</p>
+              </body>
+            </html>
+          `;
 
-      response.end(template);
-    });    
+          response.writeHead(200);
+          response.end(template);
+        });        
+      });    
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
+    
  
 });
 app.listen(3000);
